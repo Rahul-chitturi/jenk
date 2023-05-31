ae8ec37dabb753ccd7f00ba95005516a29d9e78b
RAHUL CHITTURI <57553666+Rahul-chitturi@users.noreply.github.com>
2023-05-31 17:43:41 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,1 +1,22 @@
 print("hel")
+import xml.etree.ElementTree as ET
+
+# Load the XML file
+tree = ET.parse('standardResults.xml')
+
+# Get the root element
+root = tree.getroot()
+
+average_elements = root.findall('.//average')
+
+# Calculate the average value
+total_sum = 0
+count = 0
+for average_element in average_elements:
+    average_value = int(average_element.text)
+    total_sum += average_value
+    count += 1
+
+average = total_sum / count
+
+print("Average of the 'average' column:", average)
